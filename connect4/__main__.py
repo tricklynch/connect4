a407e19f879345dfcef52943ead8331ab6c42f5d
Patrick Lynch <p@tricklynch.org>
2022-07-24 10:27:16 -0400

    Expanded module game interface support

--- connect4/__main__.py
+++ connect4/__main__.py
@@ -1,17 +1,14 @@
-from connect4 import Board
-from connect4.connect4 import Piece
+from argparse import ArgumentParser
+from importlib import import_module
 
+interface_type = {"term"}
 
 if __name__ == "__main__":
-    board = Board()
-    turn = Piece.BLACK
-    while (winner := board.get_winner()) is None:
-        try:
-            print(str(board))
-            print("".join(str(i) for i in range(len(str(board).splitlines()[0]))))
-            move = int(input(f"{turn.name}'s turn: "))
-            board.add_piece(turn, move)
-            turn = next(turn)
-        except Exception as e:
-            print(f"Invalid move: {type(e)}: {e}")
-    print(f"{winner.name} wins!")
+    parser = ArgumentParser()
+    parser.add_argument("gametype")
+    args = parser.parse_args()
+    if args.gametype not in interface_type:
+        print(f"Invalid game type: {args.gametype}")
+        exit(1)
+    game = import_module(f"connect4.{args.gametype}")
+    game.play()
---
+++ connect4/term.py
@@ -0,0 +1,16 @@
+from connect4 import Board, Piece
+
+
+def play():
+    board = Board()
+    turn = Piece.BLACK
+    while (winner := board.get_winner()) is None:
+        try:
+            print(str(board))
+            print("".join(str(i) for i in range(len(str(board).splitlines()[0]))))
+            move = int(input(f"{turn.name}'s turn: "))
+            board.add_piece(turn, move)
+            turn = next(turn)
+        except Exception as e:
+            print(f"Invalid move: {type(e)}: {e}")
+    print(f"{winner.name} wins!")
